919c32718f2ac0e22161f9b0096d53ef12c96db8
Ievgen Sherstiuk <73878060+IevgenSherstiuk@users.noreply.github.com>
2023-12-23 19:51:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ from inductor_my import N_culculate
 from pprint import pprint
 from styles import cell_style
 from btns_handler import btns_from_df, update_btns
+import sys
+
+if sys.platform == "emscripten": # check if run in Pyodide environment
+    import micropip
+    await micropip.install("regex")
+
+
+
 
 def main(page: ft.Page):
     page.title = "Калькулятор цены дроселя"
