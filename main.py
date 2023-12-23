a4aadcd0afe85ecace52b73ed6c39662b1d6a5a8
Ievgen Sherstiuk <73878060+IevgenSherstiuk@users.noreply.github.com>
2023-12-23 22:01:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 import flet as ft
+import sys
+
+if sys.platform == "emscripten": # check if run in Pyodide environment
+    import micropip
+    await micropip.install("pandas")
+
+import pandas
+
+Nvalue_dic = {
+        '5': [2500, 3200, 2500, 2000, 2000, 2100, 2250, 2300],
+        '10': [1500, 2200, 4500, 2000, 2500, 3000, 3000, 3200]
+    }
+
+
+df = pandas.DataFrame.from_dict(Nvalue_dic)
+
+print(df.loc[0, '5'])
 
 def main(page: ft.Page):
     page.title = "Flet counter example"
